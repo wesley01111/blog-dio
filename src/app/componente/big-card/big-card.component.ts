08c81d70d2ba934756d049ed5f826b0abb6cfbae
wesley <euweslei17@gmail.com>
2024-02-03 15:39:41 -0300

    parte inteligente implantada

--- src/app/componente/big-card/big-card.component.ts
+++ src/app/componente/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
-
+@Input()
+foto:string=""
+@Input()
+titulo:string=""
+@Input()
+descricao:string=""
 }
--- src/app/componente/smal-card/smal-card.component.ts
+++ src/app/componente/smal-card/smal-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-smal-card',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './smal-card.component.css'
 })
 export class SmalCardComponent {
-
+@Input()
+  foto:string=""
+  @Input()
+  descricao:string=""
 }
